4c6ea5096b462d7ce0cabf92eaad10d4dfb84e78
Kyle Reilly <kylereilly@quickenloans.com>
2020-05-11 16:12:28 -0400

    Fixed line endings in prepare-s3.sh

--- docker/prepare_s3.sh
+++ docker/prepare_s3.sh
@@ -6,8 +6,7 @@ status=$(curl -XGET s3proxy/common-voice-corpus --write-out "%{http_code}\n" --o
 
 echo "GET bucket code: $status"
 
-if [ $status != "200" ]
-then
+if [ $status != "200" ]; then
     echo "Bucket does not exist. Creating one..."
     status=$(curl -XPUT s3proxy/common-voice-corpus --write-out "%{http_code}\n" --output /dev/null --silent)
     echo "PUT bucket code: $status"
